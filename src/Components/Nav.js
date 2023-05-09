12dec340e9421b693ce0d8e54c951b4010848c5f
ronJohnPursuit <ronjohnson@pursuit.org>
2023-05-09 12:50:51 -0400

    home page setup

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import logo from "../Assets/LOGO.png";
 import "./Nav.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Nav() {
-    return (
-        <nav>
-            <img src={logo} alt="logo" height="100px" />
-            <Link to="/">
-                Sign-in
-            </Link>
-        </nav>
-    )
+  return (
+    <nav>
+      <RxHamburgerMenu size={"50px"} color={"#41CDBC"} />
+      <img src={logo} alt="logo" height="90%" />
+      <Link to="/">Sign-in</Link>
+    </nav>
+  );
 }
