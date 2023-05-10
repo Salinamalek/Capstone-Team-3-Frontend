9c6d4af158aed6a99d4df4b4f99eb9b17fa384b6
DestinyJoyner <destinyjoyner@pursuit.org>
2023-05-09 22:19:29 -0400

    minor fix

--- src/Components/App/Nav.js
+++ src/Components/App/Nav.js
@@ -1,6 +1,9 @@
 import { useState} from "react";
 import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineHome } from "react-icons/ai"
+import { MdWorkOutline } from "react-icons/md"
+import { BiInfoCircle, BiCopyright } from "react-icons/bi"
 import logo from "../../Assets/LOGO.png";
 import "./Nav.css";
 
@@ -12,40 +15,53 @@ export default function Nav() {
     setOpenNav(!openNav)
   }
 
-
   return (
     <nav>
-      
       <RxHamburgerMenu 
       size={"50px"} 
       color={"#41CDBC"}
       onClick = {() => navbarClick()} />
 
-      <img src={logo} alt="logo" height="90%" />
+      <img 
+      src={logo} 
+      alt="logo" 
+      height="90%" />
 
       <Link to="/">Sign-in</Link>
 
       {/* sliding nav bar section */}
-        <aside className={ openNav ? " slide-nav nav-open" : " slide-nav nav-close"}>
-        <p>
-          <span>inIT</span>
-          <br/>
-          <span className="slogan">"Your first tech opportunity awaits..."</span>
-        </p>
-        <Link to = "/" onClick = {() => navbarClick()} >Home</Link>
-        <Link to = "/jobs" onClick = {() => navbarClick()}>Jobs</Link>
-        <Link to = "/about" onClick = {() => navbarClick()}>About</Link>
-        <button>LOGOUT</button>
-        {/* maybe have footer info here ??  */}
-        <div className="footer-info">
-          <span>inIT</span>
-            <span>Team 3 Capstone</span>
-            <span>May 2023</span>
-            <span>
-              <a href = "https://www.pursuit.org/" target = "_blank" className="pursuit">Pursuit.org</a> 
-            </span>
-        </div>
-      </aside>
+      <aside 
+      className={ openNav ? " slide-nav nav-open" : " slide-nav nav-close"}>
+      <p>
+        <span>inIT</span>
+        <br/><br/>
+        <span className="slogan">
+          "Your first tech opportunity awaits..."
+        </span>
+      </p>
+      <Link to ="/" onClick = {() => navbarClick()} >
+        <AiOutlineHome size = {"30px"} color = {"#41cdbc"} />
+        <span>Home</span>
+      </Link>
+      <Link to ="/jobs" onClick = {() => navbarClick()}>
+        <MdWorkOutline size = {"30px"} color = {"#41cdbc"} />
+        <span>Jobs</span>
+      </Link>
+      <Link to ="/about" onClick = {() => navbarClick()}>
+        <BiInfoCircle size = {"30px"} color = {"#41cdbc"} />
+        <span>About</span>
+      </Link>
+      <button>LOGOUT</button>
+      {/* maybe have footer info here ??  */}
+      <div className="footer-info">
+        <span> inIT <BiCopyright /></span>
+        <span>Team 3 Capstone</span>
+        <span>May 2023</span>
+        <span>
+          <a href = "https://www.pursuit.org/" target = "_blank" className="pursuit">Pursuit.org</a> 
+        </span>
+      </div>
+    </aside>
       
     </nav>
   );
