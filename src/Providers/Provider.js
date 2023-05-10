38e2d1878df52484de021e0bc3ba7bd15aab76b2
DestinyJoyner <destinyjoyner@pursuit.org>
2023-05-09 20:12:12 -0400

    added API to provider

--- src/Providers/Provider.js
+++ src/Providers/Provider.js
@@ -7,11 +7,14 @@ export function useContextProvider(){
     return useContext(ContextData)
 }
 
+// API from .env
+const API = process.env.REACT_APP_API_URL
+
 function Provider({children}) {
 
     return (
         <ContextData.Provider value = {{
-    
+            API,
         }}>
             <Nav />
             {children}
