863524217833e7d695ba684ae36de830b26aa2c2
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-05-15 19:51:20 -0400

    display user details on page and some styling

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useContextProvider } from "../../Providers/Provider.js";
 import axios from "axios";
+import { VscAccount } from "react-icons/vsc"
 import "./UserProfile.css";
 
 export default function UserProfile() {
@@ -24,8 +25,32 @@ export default function UserProfile() {
       });
   }, [userID]);
   return (
-    <div>
-      User Profile
+    <div className="profile">
+    <div className="profile-details">
+      <p>Name</p>
+      <p className="bold">{user["first_name"] + " " + user["last_name"]}</p>
+      <br/>
+      <p>School</p>
+      <p className="bold">{user.school}</p>
+      <br/>
+      <p>Skills and Technologies</p>
+      <ul>{user.skills && user.skills.map((e, i) => <li key={i} className="bold">{e}</li>)}</ul>
+      <br/>
+      <p>Portfolio projects</p>
+      <ul>
+        <li className="bold">{user["project_one"] || "add a link"}</li>
+        <li className="bold">{user["project_two"] || "add a link"}</li>
+      </ul>
+      <br/>
+      <p>About me</p>
+      <p className="bold">{user.bio}</p>
+      <br />
+      <button className="profile-button">Logout</button>
+    </div>
+    <div className="right-side-profile">
+        <VscAccount id="profile-icon" size={"120px"}/>
+        <button className="profile-button">edit</button>
+    </div>
     </div>
   );
 }
