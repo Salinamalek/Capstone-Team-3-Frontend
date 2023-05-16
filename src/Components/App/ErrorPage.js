8cf92125db3d1aa18438bedaeeb0195f17a8df13
Salinamalek <salinamalek@pursuit.org>
2023-05-16 10:33:43 -0400

    error page started

--- src/Components/App/ErrorPage.js
+++ src/Components/App/ErrorPage.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import "/Users/Salina/Documents/9.1/module-six/project/capstone/capstone-frontend/Capstone-Team-3-Frontend/src/Components/App/ErrorPage.css";
+import error from "/Users/Salina/Documents/9.1/module-six/project/capstone/capstone-frontend/Capstone-Team-3-Frontend/src/Assets/404page.png";
 
 function ErrorPage() {
-    return (
-        <div className="error">
-            <Link to = "/"> Back to Home </Link>
-        </div>
-    );
+  return (
+    <div className="error">
+      <img id="error" src={error} alt="error"></img>
+      <br />
+      <Link className="backHome" to="/">
+        {" "}
+        Back to Home{" "}
+      </Link>
+    </div>
+  );
 }
 
 export default ErrorPage;
