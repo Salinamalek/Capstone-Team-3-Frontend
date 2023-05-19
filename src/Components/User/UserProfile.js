5988cc33320e29cf7e86c8e710b8159b51d519ce
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-05-19 18:30:10 -0400

    user profile css

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,10 +1,10 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useUserProvider } from "../../Providers/UserProvider.js";
-import userIcon from "../../Assets/USER.png"
+import userIcon from "../../Assets/USER.png";
+import pencil from "../../Assets/pencil.png"
 import "./UserProfile.css";
 
 export default function UserProfile() {
-
   const navigate = useNavigate();
   const { userProfile, userJobs } = useUserProvider();
 
@@ -15,59 +15,86 @@ export default function UserProfile() {
 
   return (
     <div>
-    {userProfile.id && <div className="profile">
-      <div className="left-side-profile">
-        <div className="profile-details">
-          <p>Name</p>
-          <p className="bold">{userProfile["first_name"] + " " + userProfile["last_name"]}</p>
-          <br />
-          <p>Education</p>
-          <p className="bold">{userProfile.education}</p>
-          <br />
-          <p>Portfolio projects</p>
-          <ul>
-            <li className="bold">{userProfile["project_one"]? <a href={userProfile["project_one"]} target="_blank">link 1</a>: "add link"}</li>
-            <li className="bold">{userProfile["project_two"]?<a href={userProfile["project_two"]} target="_blank">link 2</a>: "add link"}</li>
-          </ul>
-        </div>
-      </div>
-      <div className="right-side-profile">
-        <img id="user-icon" src={userIcon} alt="user icon" size="40px"/>
-        <button onClick={() => navigate(`/user/edit`)} className="profile-button">
-          edit
-        </button>
-        <p className="skills">Skills and Technologies</p>
-        <ul>
-          {userProfile.skills &&
-            userProfile.skills.map((e, i) => (
-              <li key={i} className="bold">
-                {e}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div id="bio">
-        <p>About me</p>
-        <br />
-        <p className="bold bio-box">{userProfile.bio}</p>
-      </div>
-      <div className="activity">
-        <p className="bold">My Applications</p>
-        <div>
-          {userJobs.length > 0 &&
-            userJobs.map(({ id, title, company, date_applied }) => (
-              <p key={id}>
-                <br />
-                <strong>{title}</strong> - <em>{company}</em>
-                <br />
-                applied: {dateFormat(date_applied)}
+      {userProfile.id && (
+        <div className="profile">
+          <div className="top-profile">
+            <div>
+              <p>Name</p>
+              <p className="bold">
+                {userProfile["first_name"] + " " + userProfile["last_name"]}
               </p>
-            ))}
-            <br/>
-            <button id="activity-button">{userJobs.length > 0? "view all applications" : "view jobs"}</button>
+              <br />
+              <p>Education</p>
+              <p className="bold">{userProfile.education}</p>
+              <br/>
+              <p className="skills">Skills and Technologies</p>
+          <ul>
+            {userProfile.skills &&
+              userProfile.skills.map((e, i) => (
+                <li key={i} className="bold">
+                  {e}
+                </li>
+              ))}
+          </ul>
+            </div>
+            <div className="icon-edit">
+              <img id="icon-user" src={userIcon} alt="user icon" />
+              <button
+                onClick={() => navigate(`/user/edit`)}
+                className="profile-button"
+              >
+                EDIT <img src={pencil} alt="pencil" />
+              </button>
+            </div>
+          </div>
+          <br />
+          <p>Portfolio Projects</p>
+          <ul className="portfolio">
+            <li className="bold">
+              {userProfile["project_one"] ? (
+                <a href={userProfile["project_one"]} target="_blank">
+                  LINK 1
+                </a>
+              ) : (
+                "add link"
+              )}
+            </li>
+            <li className="bold">
+              {userProfile["project_two"] ? (
+                <a href={userProfile["project_two"]} target="_blank">
+                  LINK 2
+                </a>
+              ) : (
+                "add link"
+              )}
+            </li>
+          </ul>
+          <br/>
+          <div id="bio">
+            <p>About me</p>
+            <p className="bold bio-box">{userProfile.bio}</p>
+          </div>
+          <br/>
+          <div className="applications">
+            <p className="bold">My Applications</p>
+            <div>
+              {userJobs.length > 0 &&
+                userJobs.map(({ id, title, company, date_applied }, i) => (
+                  i < 2 ? <p key={id}>
+                    <br />
+                    <strong>{title}</strong> - <em>{company}</em>
+                    <br />
+                    applied: {dateFormat(date_applied)}
+                  </p> : null
+                ))}
+              <br />
+              <button id="activity-button">
+                {userJobs.length > 0 ? "VIEW ALL" : "view jobs"}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>}
+      )}
     </div>
   );
 }
