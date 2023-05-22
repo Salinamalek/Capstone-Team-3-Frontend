3bd036fe8aab0dc386e742dc4b5d08f5865f25c7
DestinyJoyner <destinyjoyner@pursuit.org>
2023-05-21 20:12:39 -0400

    searchbar

--- src/Components/Job/JobsIndex.js
+++ src/Components/Job/JobsIndex.js
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 import { useJobProvider } from "../../Providers/JobProvider.js";
 import { v4 as uuidv4 } from 'uuid';
 import JobsCard from "./JobsCard";
-import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar.js";
 import "./JobsIndex.css"
 
 
 function JobsIndex() {
-   const { API, axios, jobs } = useJobProvider()
+   const { jobs, searchResult } = useJobProvider()
 
    // jobs provider needed for searchbar/ filter bar to have cascade access for all jobs axios call
 
--- src/Components/Job/SearchBar.js
+++ src/Components/Job/SearchBar.js
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import FilterBar from "./FilterBar";
-import { CiSearch } from "react-icons/ci"
+import { useState } from 'react'
+import { useJobProvider } from '../../Providers/JobProvider'
+import FilterBar from './FilterBar'
 import "./SearchBar.css"
 
 function SearchBar() {
-    const [search, setSearch] = useState("")
+    const {jobs, setJobs, searchResult, setSearchResult} = useJobProvider()
+    const[search, setSearch] = useState("")
+    
+    function handleSearchBar(e) {
+        const value = e.target.value
+        setSearch(value)
+        const filt = searchResult.filter(({title}) =>{
+            const regex = new RegExp(search,"gi")
+            return title.match(regex)
+        } )
+        setJobs(filt)
+
+        
+    }
 
     return (
         <section className="search-component" >
@@ -13,7 +26,7 @@ function SearchBar() {
             type= "text"
             value={search}
             placeholder="Search Jobs..."
-            onChange={(event) => setSearch(event.target.value)}
+            onChange={(event) => handleSearchBar(event)}
             
             />
             {/* <CiSearch size ={"28px"} color={"#41CDBC"} className="search-bar-icon" /> */}
