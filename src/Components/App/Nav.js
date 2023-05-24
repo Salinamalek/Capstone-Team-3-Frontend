026e69a66d00550b8b55ab44df9f35c1625d5bb8
DestinyJoyner <destinyjoyner@pursuit.org>
2023-05-24 13:40:00 -0400

    dark mode toggle fix

--- src/Components/App/Nav.js
+++ src/Components/App/Nav.js
@@ -14,16 +14,25 @@ import logo from "../../Assets/LOGO.png";
 import "./Nav.css";
 
 export default function Nav() {
-  const [openNav, setOpenNav] = useState(false);
-  const navigate = useNavigate();
   const { theme, setTheme, isSignedIn, setIsSignedIn, API, axios, userID } =
-    useContextProvider();
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
+  useContextProvider();
+
+  const [openNav, setOpenNav] = useState(false);
+  const [isChecked, setIsChecked] = useState(localStorage.getItem("theme") === "dark" ? true : false )
+  const navigate = useNavigate();
+ 
+
+  const toggleTheme = (e) => {
+    const checkbox = e.target.checked
+    if(checkbox){
+      setTheme("dark")
+      setIsChecked(true)
     }
+    else {
+      setTheme("light")
+      setIsChecked(false)
+    }
+  
   };
   useEffect(() => {
     const handleScroll = () => {
@@ -81,23 +90,15 @@ export default function Nav() {
 
       <img src={logo} alt="logo" />
 
-      {/* <Link to="/">
-        <VscAccount className="nav-sign" size={"30px"} />
-        <br />
-        Sign-in
-      </Link> */}
-
       {/* sliding nav bar section */}
       <aside
         className={openNav ? " slide-nav nav-open" : " slide-nav nav-close"}
       >
         <p>
-          {/* <span>inIT</span> */}
           <br />
 
           <span className="slogan">"Your first tech opportunity awaits!"</span>
         </p>
-        {/* DESTINY adding register/ login/ user/4 route for easy navigation while testing */}
         {!isSignedIn && (
           <Link to="/user" onClick={() => loginClick()}>
             <FiLogIn size={"30px"} color={"#41cdbc"} />
@@ -129,14 +130,11 @@ export default function Nav() {
           <BiInfoCircle size={"30px"} color={"#41cdbc"} />
           <span>Meet the Team</span>
         </Link>
-        {/* <Link to="/user/4" onClick={() => navbarClick()}>
-          <GrAlert size={"30px"} color={"#41cdbc"} />
-          <span>User Profile</span>
-        </Link> */}
-        {/* <hr className="nav-line"></hr> */}
-        {/* was class => to className */}
         <label className="switch">
-          <input type="checkbox" onChange={toggleTheme} />
+          <input 
+          type="checkbox" 
+          checked={isChecked}
+          onChange={(event) => toggleTheme(event)} />
           <span className="slider round"></span>
         </label>
         {isSignedIn && (
@@ -144,16 +142,6 @@ export default function Nav() {
             {<FiLogOut />} Logout
           </button>
         )}
-
-        {/* maybe have footer info here ??  */}
-        {/* <div className="footer-info">
-        <span> inIT <BiCopyright /></span>
-        <span>Team 3 Capstone</span>
-        <span>May 2023</span>
-        <span>
-          <a href = "https://www.pursuit.org/" target = "_blank" className="pursuit">Pursuit.org</a> 
-        </span>
-      </div> */}
       </aside>
     </nav>
   );
