22589da29a60b0314d1ad97fcbfc0455fbb3fd71
Salinamalek <salinamalek@pursuit.org>
2023-05-26 18:13:48 -0400

    updated about page

--- src/Components/App/AboutPage.js
+++ src/Components/App/AboutPage.js
@@ -45,13 +45,13 @@ function AboutPage(props) {
           <img
             id="4"
             className="devicon"
-            src="https://avatars.githubusercontent.com/u/105683843?v=4"
+            src="https://avatars.githubusercontent.com/u/105737822?v=4"
             onClick={(event) => aboutCard(event)}
           ></img>
           <img
             id="5"
             className="devicon"
-            src="https://avatars.githubusercontent.com/u/105737822?v=4"
+            src="https://avatars.githubusercontent.com/u/105683843?v=4"
             onClick={(event) => aboutCard(event)}
           ></img>
         </div>
@@ -64,125 +64,11 @@ function AboutPage(props) {
             {profileCard.links.length > 0 &&
               profileCard.links.map((el, i) => (
                 <a href={el} target="_blank">
-                  {iconArr[i]}{" "}
+                  {iconArr[i]}
                 </a>
               ))}
           </div>
         </div>
-
-        {/* <div className="indiv-card">
-          <h2>Dan Mazzilli</h2>
-          <img
-            className="dev-img"
-            src="https://avatars.githubusercontent.com/u/107490157?v=4"
-          ></img>
-          <p>
-            Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisum
-            eum dolores molestiae qui tempora vitae sed dolor galisum. Et earum
-            magnam ut provident laborum et officiis asperiores ea animi
-            dignissimos aut autem reprehenderit ut quidem architecto At amet
-            nulla.
-          </p>
-          <hr className="socials-line"></hr>
-          <div className="dev-socials">
-            <a
-              href="https://www.linkedin.com/in/mazzilli-daniel/"
-              target="_blank"
-            >
-              <AiFillLinkedin />
-            </a>
-            <a href="https://github.com/Daniel-Mazzilli" target="_blank">
-              <BsGithub />
-            </a>
-            <a href="mailto:danmazzilli@pursuit.org">
-              <IoMdMail />
-            </a>
-          </div>
-        </div>
-        <div className="indiv-card">
-          <h2>Salina Malek</h2>
-          <img
-            className="dev-img"
-            src="https://avatars.githubusercontent.com/u/105737474?v=4"
-          ></img>
-          <p>
-            Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisum
-            eum dolores molestiae qui tempora vitae sed dolor galisum. Et earum
-            magnam ut provident laborum et officiis asperiores ea animi
-            dignissimos aut autem reprehenderit ut quidem architecto At amet
-            nulla.
-          </p>
-          <hr className="socials-line"></hr>
-          <div className="dev-socials">
-            <a href="https://www.linkedin.com/in/salina-malek/" target="_blank">
-              <AiFillLinkedin />
-            </a>
-            <a href="https://github.com/Salinamalek" target="_blank">
-              <BsGithub />
-            </a>
-            <a href="mailto:salinamalek@pursuit.org">
-              <IoMdMail />
-            </a>
-          </div>
-        </div>
-        <div className="indiv-card">
-          <h2>Jahaad Petty</h2>
-          <img
-            className="dev-img"
-            src="https://avatars.githubusercontent.com/u/105683843?v=4"
-          ></img>
-          <p>
-            Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisum
-            eum dolores molestiae qui tempora vitae sed dolor galisum. Et earum
-            magnam ut provident laborum et officiis asperiores ea animi
-            dignissimos aut autem reprehenderit ut quidem architecto At amet
-            nulla.
-          </p>
-          <hr className="socials-line"></hr>
-          <div className="dev-socials">
-            <a
-              href="https://www.linkedin.com/in/jahaad-petty-321066249/"
-              target="_blank"
-            >
-              <AiFillLinkedin />
-            </a>
-            <a href="https://github.com/PESolut" target="_blank">
-              <BsGithub />
-            </a>
-            <a href="mailto:jahaadpetty@pursuit.org">
-              <IoMdMail />
-            </a>
-          </div>
-        </div>
-        <div className="indiv-card">
-          <h2>Ron Johnson</h2>
-          <img
-            className="dev-img"
-            src="https://avatars.githubusercontent.com/u/105737822?v=4"
-          ></img>
-          <p>
-            Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisum
-            eum dolores molestiae qui tempora vitae sed dolor galisum. Et earum
-            magnam ut provident laborum et officiis asperiores ea animi
-            dignissimos aut autem reprehenderit ut quidem architecto At amet
-            nulla.
-          </p>
-          <hr className="socials-line"></hr>
-          <div className="dev-socials">
-            <a
-              href="https://www.linkedin.com/in/ronald-johnson-97b22830/"
-              target="_blank"
-            >
-              <AiFillLinkedin />
-            </a>
-            <a href="https://github.com/ronJohnPursuit" target="_blank">
-              <BsGithub />
-            </a>
-            <a href="mailto:ronjohnson@pursuit.org">
-              <IoMdMail />
-            </a>
-          </div>
-        </div> */}
       </div>
     </div>
   );
--- src/Functions/AboutData.js
+++ src/Functions/AboutData.js
@@ -1,23 +1,60 @@
 import userIcon from "../Assets/USER.png";
-import tempLogo from "../Assets/LOGO.png";
 
 export const team = {
   1: {
     id: 1,
-    name: "Destiny",
+    name: "Destiny Joyner",
     img: userIcon,
     bio: "Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisumeum dolores molestiae qui tempora vitae sed dolor galisum. Et earummagnam ut provident laborum et officiis asperiores ea animidignissimos aut autem reprehenderit ut quidem architecto At ametnulla.",
-    links: ["", "", ""],
+    links: [
+      "https://github.com/DestinyJoyner",
+      "https://www.linkedin.com/in/destinyjoyner/",
+      "mailto:destinyjoyner@pursuit.org",
+    ],
   },
   2: {
     id: 2,
-    name: "Dan",
-    img: "",
-    bio: "",
-    links: ["", "https://www.linkedin.com/in/mazzilli-daniel/", ""],
+    name: "Dan Mazzilli",
+    img: userIcon,
+    bio: "Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisumeum dolores molestiae qui tempora vitae sed dolor galisum. Et earummagnam ut provident laborum et officiis asperiores ea animidignissimos aut autem reprehenderit ut quidem architecto At ametnulla.",
+    links: [
+      "https://github.com/Daniel-Mazzilli",
+      "https://www.linkedin.com/in/mazzilli-daniel/",
+      "mailto:danmazzilli@pursuit.org",
+    ],
   },
-  3: { id: 3, name: "Salina", img: "", bio: "", links: ["", "", ""] },
-  4: { id: 4, name: "JP", img: "", bio: "", links: ["", "", ""] },
-  5: { id: 5, name: "Ron", img: "", bio: "", links: ["", "", ""] },
-  6: { id: 6, name: "Select a Team Member", img: tempLogo, bio: "", links: [] },
+  3: {
+    id: 3,
+    name: "Salina Malek",
+    img: userIcon,
+    bio: "Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisumeum dolores molestiae qui tempora vitae sed dolor galisum. Et earummagnam ut provident laborum et officiis asperiores ea animidignissimos aut autem reprehenderit ut quidem architecto At ametnulla.",
+    links: [
+      "https://github.com/Salinamalek",
+      "https://www.linkedin.com/in/salina-malek/",
+      "mailto:salinamalek@pursuit.org",
+    ],
+  },
+  4: {
+    id: 4,
+    name: "Ron Johnson",
+    img: userIcon,
+    bio: "Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisumeum dolores molestiae qui tempora vitae sed dolor galisum. Et earummagnam ut provident laborum et officiis asperiores ea animidignissimos aut autem reprehenderit ut quidem architecto At ametnulla.",
+    links: [
+      "https://github.com/ronJohnPursuit",
+      "https://www.linkedin.com/in/ronald-johnson-97b22830/",
+      "mailto:ronjohnson@pursuit.org",
+    ],
+  },
+  5: {
+    id: 5,
+    name: "Jahaad Petty",
+    img: userIcon,
+    bio: "Lorem ipsum dolor sit amet. Rem provident illo ut reiciendis galisumeum dolores molestiae qui tempora vitae sed dolor galisum. Et earummagnam ut provident laborum et officiis asperiores ea animidignissimos aut autem reprehenderit ut quidem architecto At ametnulla.",
+    links: [
+      "https://github.com/PESolut",
+      "https://www.linkedin.com/in/jahaad-petty-321066249/",
+      "mailto:jahaadpetty@pursuit.org",
+    ],
+  },
+  6: { id: 6, name: "Select a Team Member", img: userIcon, bio: "", links: [] },
 };
