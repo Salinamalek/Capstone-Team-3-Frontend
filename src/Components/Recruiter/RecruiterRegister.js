5250b2be5148654daa2cbc3e1f99b8dc7e1480e5
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-06-04 13:49:40 -0400

    working on register/login rec

--- src/Components/Recruiter/RecruiterRegister.js
+++ src/Components/Recruiter/RecruiterRegister.js
@@ -15,48 +15,65 @@ export default function RecruiterRegister() {
     setUserID,
   } = useRecruiterProvider();
   const [newAccountForm, setNewAccountForm] = useState({
-    first_name: "",
-    last_name: "",
-    school: "",
-    organization: "",
-    email: "",
-    password: "",
-    isRecruiter: "",
+    profile: { first_name: "", last_name: "", education: "", organization: "" },
+    login: {
+      email: "",
+      password: "",
+      isRecruiter: "",
+    },
   });
 
   return (
     <div className="recruiter-register">
       <h1>Sign up</h1>
+      <Link to="/recruiter/login">Already have an account? Log in</Link>
       <form className="recruiter-register-form">
-        <select id="isRecruiter">
+        <select id="login.isRecruiter">
           <option>Select Account Type</option>
           <option value={false}>Applicant</option>
           <option value={true}>Recruiter</option>
         </select>
-        <label htmlFor="first_name">First name<span>*</span></label>
-        <input id="first_name" type="text" required />
-        <label htmlFor="last_name">Last name<span>*</span></label>
-        <input id="last_name" type="text" required />
+        <label htmlFor="profile.first_name">
+          First name<span>*</span>
+        </label>
+        <input id="profile.first_name" type="text" required />
+        <label htmlFor="profile.last_name">
+          Last name<span>*</span>
+        </label>
+        <input id="profile.last_name" type="text" required />
         <label
-          htmlFor={newAccountForm.isRecruiter ? "organization" : "education"}
+          htmlFor={newAccountForm.isRecruiter ? "profile.organization" : "profile.education"}
         >
           {newAccountForm.isRecruiter === ""
             ? "Education / Organization"
             : newAccountForm.isRecruiter
             ? "Organization"
-            : "Education"}<span>*</span>
+            : "Education"}
+          <span>*</span>
         </label>
         <input
-          id={newAccountForm.isRecruiter ? "organization" : "education"}
+          id={newAccountForm.isRecruiter ? "profile.organization" : "profile.education"}
           type="text"
           required
         />
-         <label htmlFor="email">Email<span>*</span></label>
-        <input id="email" type="email" required />
-        <label htmlFor="password">Password<span>*</span></label>
-        <input id="password" type="password" required />
-        <p className="register-helper-text">Include one lowercase, uppercase, numerical, and special symbol. At least 5 character lenght</p>
-        <input id="recruiter-register-submit" type="submit" value="SUBMIT"/>
+        <label htmlFor="login.email">
+          Email<span>*</span>
+        </label>
+        <input id="login.email" type="email" required />
+        <label htmlFor="login.password">
+          Password<span>*</span>
+        </label>
+        <input
+          className="register-input-pass"
+          id="login.password"
+          type="password"
+          required
+        />
+        <p className="register-helper-text">
+          Include one lowercase, uppercase, numerical, and special symbol. At
+          least 5 character lenght
+        </p>
+        <input id="recruiter-register-submit" type="submit" value="SUBMIT" />
       </form>
     </div>
   );
