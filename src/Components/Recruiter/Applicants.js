82e84d8d195a86c79f1403324ca1f1ff681a1648
DestinyJoyner <destinyjoyner@pursuit.org>
2023-06-04 21:18:37 -0400

    dark mode css for applicants

--- src/Components/Recruiter/Applicants.js
+++ src/Components/Recruiter/Applicants.js
@@ -1,61 +1,60 @@
-import { useState, useEffect } from "react"
-import { Navigate, useNavigate } from "react-router-dom";
-import { useJobProvider } from "../../Providers/JobProvider"
-import {v4 as uuidv4} from "uuid"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useJobProvider } from "../../Providers/JobProvider";
+import { v4 as uuidv4 } from "uuid";
 import ApplicantCard from "./ApplicantCard";
 import { jobCompany, jobLocation } from "../Job/Data/Icons";
 import { TfiAngleLeft } from "react-icons/tfi";
-import "./Applicants.css"
+import "./Applicants.css";
 import { Link } from "react-router-dom";
 
 export default function Applicants() {
-    const {recruiterJobs, recruiterID, access, jobID} = useJobProvider()
-    const navigate = useNavigate()
-    const [applicants, setApplicants] = useState([])
-    const [thisJob, setThisJob] = useState({})
-    const [noData, setNoData] = useState(false)
+  const { recruiterJobs, recruiterID, access, jobID } = useJobProvider();
+  const navigate = useNavigate();
+  const [applicants, setApplicants] = useState([]);
+  const [thisJob, setThisJob] = useState({});
 
-    useEffect(() => {
-        const filter = recruiterJobs.find(({id}) => id === +jobID )
-        if(filter){
-            setThisJob(filter)
-        }
-        if(filter && filter.users){
-            setApplicants(filter.users)
-        }
-        else {
-            setNoData(true)
-        }
-        
-    }, [jobID, recruiterJobs.length])
 
-    return (
-        <div className="job-applicant-page">
-            <section className="job-applicant-header">
-                <TfiAngleLeft onClick={() => navigate(-1)} size={"30px"} />
-                <h2 className="applicant-title">{thisJob.title}</h2>
-                <span className="applicant-company">{jobCompany}{thisJob.company}</span>
-                <span className="applicant-city">{jobLocation}{thisJob.city}</span>
-                <span className="applicant-remote">{thisJob.full_remote && "REMOTE"}</span>
-            </section>
+  useEffect(() => {
+    const filter = recruiterJobs.find(({ id }) => id === +jobID);
+    if (filter) {
+      setThisJob(filter);
+    }
+    if (filter && filter.users) {
+      setApplicants(filter.users);
+    } 
+  }, [jobID, recruiterJobs.length]);
 
-            <section className="applicant-list">
-                <h2>{!applicants.length && "No "}Job Applicants</h2>
-                {
-                    applicants.map(obj => 
-                        <ApplicantCard 
-                        key={uuidv4()}
-                        obj ={obj}
-                        />)
-                }
-            </section>
+  return (
+    <div className="job-applicant-page">
+      <section className="job-applicant-header">
+        <TfiAngleLeft onClick={() => navigate(-1)} size={"30px"} />
+        <h2 className="applicant-title">{thisJob.title}</h2>
+        <span className="applicant-company">
+          {jobCompany}
+          {thisJob.company}
+        </span>
+        <span className="applicant-city">
+          {jobLocation}
+          {thisJob.city}
+        </span>
+        <span className="applicant-remote">
+          {thisJob.full_remote && "REMOTE"}
+        </span>
+      </section>
 
-            <Link 
-            className="job-applicant-link"
-            to ={`/jobs/${thisJob.id}`}>
-                VIEW JOB DETAILS
-            </Link>
-            
-        </div>
-    )
+      <section className="applicant-list">
+        <h2>{!applicants.length && "No "}Job Applicants</h2>
+        {applicants.map((obj) => (
+          <ApplicantCard key={uuidv4()} obj={obj} />
+        ))}
+      </section>
+
+      <Link 
+      className="job-applicant-link" 
+      to={`/jobs/${thisJob.id}`}>
+        VIEW JOB DETAILS
+      </Link>
+    </div>
+  );
 }
