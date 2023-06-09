b985efbdc73052f5762ad9bda6ad788a7c7ef617
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-06-09 00:00:45 -0400

    working on css and func for register two

--- src/Components/Recruiter/RecruiterRegister2.js
+++ src/Components/Recruiter/RecruiterRegister2.js
@@ -1,30 +1,82 @@
+import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { useUserProvider } from "../../Providers/UserProvider.js";
 import Header from "../Job/Header.js";
 import SkillsComponent from "../Job/SkillsComponent.js";
 import "./RecruiterRegister2.css";
 
 export default function RecruiterRegister2() {
   const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState({});
+  const [updateSkills, setUpdateSkills] = useState([]);
+  const { API, axios, userID } = useUserProvider();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/users/${userID}`)
+      .then(({ data }) => setUserDetails(data))
+      .catch((err) => console.log(err));
+  }, [userID]);
+
+  const handleChange = (event) => {
+    setUserDetails({ ...userDetails, [event.target.id]: event.target.value });
+  };
+
+  const skillsHandle = (event) => {
+    let selectedSkills = [...updateSkills];
+    selectedSkills =
+      !selectedSkills.includes(+event.target.id) && selectedSkills.length < 4
+        ? [...selectedSkills, +event.target.id]
+        : selectedSkills.filter((e) => e !== +event.target.id);
+    setUpdateSkills(selectedSkills);
+  };
+
   return (
     <div className="registration-two">
-      <Header header={"Complete your account"} noBack={true} />
-      <h2>Account successfully created</h2>
-      <h3>Add more account details</h3>
-     
+      <Header header={"Add more account details"} noBack={true} />
       <form className="form-register-two">
-        <label>Biography</label>
-        <textarea />
-        <div>
-          <p>Portfolio Projects</p>
-          <input />
-          <input />
-        </div>
-        <SkillsComponent 
-        key={uuidv4()}
-        checkedArr={[2, 3]}
-        checkbox={true}
+        <label htmlFor="bio">About me</label>
+        <textarea
+          id="bio"
+          maxLength={160}
+          placeholder="add a short bio"
+          onChange={handleChange}
+          value={userDetails.bio || ""}
         />
+        <div className="register-projects">
+          <p>Portfolio Projects</p>
+          <input
+            id="project_one"
+            type="url"
+            placeholder="https://example.com"
+            onChange={handleChange}
+            value={userDetails.project_one || ""}
+          />
+          <input
+            id="project_two"
+            type="url"
+            placeholder="https://example.com"
+            onChange={handleChange}
+            value={userDetails.project_two || ""}
+          />
+        </div>
+        <div className="register-two-skills">
+          <p>
+            Skills & Technologies
+            <br />
+            <span>select up to 4</span>
+          </p>
+          <div className="register-skill-icons">
+            <SkillsComponent
+              key={uuidv4()}
+              checkBoxHandle={skillsHandle}
+              checkedArr={updateSkills}
+              checkbox={true}
+            />
+          </div>
+        </div>
+        <input type="submit" value="SUBMIT" />
       </form>
       <Link to="/jobs">Skip for now</Link>
     </div>
--- src/Pages/RegisterRecruiter2.js
+++ src/Pages/RegisterRecruiter2.js
@@ -1,10 +1,10 @@
-import RecruiterProvider from "../Providers/RecruiterProvider";
+import UserProvider from "../Providers/UserProvider.js";
 import RecruiterRegister2 from "../Components/Recruiter/RecruiterRegister2.js";
 
 export default function RegisterRecruiter() {
   return (
-    <RecruiterProvider>
+    <UserProvider>
       <RecruiterRegister2 />
-    </RecruiterProvider>
+    </UserProvider>
   );
 }
