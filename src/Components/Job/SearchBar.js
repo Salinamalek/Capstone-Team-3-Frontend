823cc22c1bd160612615a9ba95abf7ad06352a82
DestinyJoyner <destinyjoyner@pursuit.org>
2023-06-16 10:43:04 -0400

    skills filter fix, added delete to recruiter view

--- src/Components/Job/SearchBar.js
+++ src/Components/Job/SearchBar.js
@@ -64,7 +64,9 @@ function SearchBar() {
       const skillFilter = filterSearch.filter((obj) => {
         let includesAll = true;
         for (let i = 0; i < searchOptions.skills.length; i++) {
-          if (!obj["skill_id"].includes(searchOptions.skills[i])) {
+          console.log(obj, "skills")
+          const skillDataType = typeof obj["skill_id"] === "number" ? [obj["skill_id"]] : obj["skill_id"];
+          if (!skillDataType.includes(searchOptions.skills[i])) {
             includesAll = false;
             break;
           }
